153eee576e5e150b76c7966ef76a8b0d555348b9
wolfeh <bill@wolfeh.info>
2022-04-27 16:34:14 -0700

    4. Added testSimpleGene() method

--- Java_Programming_ Solving_Problems_with_Software/week1/perimeterAssignment/src/Part1.java
+++ Java_Programming_ Solving_Problems_with_Software/week1/perimeterAssignment/src/Part1.java
@@ -13,7 +13,7 @@ public class Part1 {
 
         // Error Check: if either index is negative, then return error
         if (startCodonIndex < 0 || endCodonIndex < 0) {
-            return "invalid start/end cod";
+            return "";
         }
 
         // Get the gene. The +3 offset is to account for the length of the end codon
@@ -21,10 +21,48 @@ public class Part1 {
 
         // Error Check: If the gene is not made up of codons, then return error
         if (gene.length() % 3 != 0) {
-            return "invalid gene";
+            return "";
         } else {
             // Return the gene
             return gene;
         }
+    }
+
+    public void testSimpleGene() {
+        // Creating test cases for error checking. 
+        // Only one test case should pass
+        
+        // No ATG
+        String testCase1 = "TAGCTGCTAGCTCAGTGACTAAGCTGCA";
+        // No TAA
+        String testCase2 = "TATGTCTGCTAGCTC";
+        // No ATG or TAA
+        String testCase3 = "TAGCTGCTAGCTCAGTGACTAGCTGCA";
+        // ATG and TAA but not multiple of 3
+        String testCase4 = "TATGCTGCTAGCTCAGTGACTAAGCTGCA";
+        // Proper ATG and TAA and multiple of 3
+        String testCase5 = "TATGCTGCTAGCTCAGTAACTGAGCTGCA";
+
+        // Print the DNA string in pairs with the search result
+        // No ATG
+        System.out.println("Gene: " + testCase1);
+        System.out.println("Result: " + findSimpleGene(testCase1));
+        // No TAA
+        System.out.println("Gene: " + testCase2);
+        System.out.println("Result: " + findSimpleGene(testCase2));
+        // No ATG or TAA
+        System.out.println("Gene: " + testCase3);
+        System.out.println("Result: " + findSimpleGene(testCase3));
+        // ATG and TAA but not multiple of 3
+        System.out.println("Gene: " + testCase4);
+        System.out.println("Result: " + findSimpleGene(testCase4));
+        // Proper ATG and TAA and multiple of 3
+        System.out.println("Gene: " + testCase5);
+        System.out.println("Result: " + findSimpleGene(testCase5));
+    }
+
+    public static void main(String[] args) throws Exception {
+        Part1 p = new Part1();
+        p.testSimpleGene();
     }
 }
