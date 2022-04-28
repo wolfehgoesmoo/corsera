c50dada16c86f2f0587a10e9269a92b05fbfd4ec
wolfeh <bill@wolfeh.info>
2022-04-27 18:55:02 -0700

    4. Updated youtube link capture to be case sensitive

--- Java_Programming_ Solving_Problems_with_Software/week2/FindingGene/src/Part4.java
+++ Java_Programming_ Solving_Problems_with_Software/week2/FindingGene/src/Part4.java
@@ -3,20 +3,30 @@ package FindingGene.src;
 import edu.duke.URLResource;
 
 public class Part4 {
-    
+
     public void readYoutubeWebLinks() {
         URLResource ur = new URLResource("https://www.dukelearntoprogram.com//course2/data/manylinks.html");
         int youtubeIndex = 0;
         int firstQuoteIndex = 0;
         int secondQuoteIndex = 0;
+        String presearchString = null;
 
         // Loop through all words in the site
         for (String s : ur.lines()) {
-            
+
+            // Set a copy of the current string so that it can be read in after. 
+            // We need to capture all cases of youtube, then restore the original string
+            presearchString = s;
+
+            s = s.toLowerCase();
+
             // If the word contains “youtube.com”
             youtubeIndex = s.indexOf("youtube.com");
             if (youtubeIndex != -1) {
 
+                // Restore the original string
+                s = presearchString;
+                
                 // Find the start of the link using " on the left
                 firstQuoteIndex = s.lastIndexOf("\"", youtubeIndex);
                 if (firstQuoteIndex != -1) {
