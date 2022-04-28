519c1fb84149ea80a657039e0f6f9350c752e75a
wolfeh <bill@wolfeh.info>
2022-04-27 18:18:25 -0700

    2. Added readYoutubeWebLinks() method

--- Java_Programming_ Solving_Problems_with_Software/week2/FindingGene/src/Part4.java
+++ Java_Programming_ Solving_Problems_with_Software/week2/FindingGene/src/Part4.java
@@ -1,5 +1,17 @@
 package FindingGene.src;
 
+import edu.duke.URLResource;
+
 public class Part4 {
-    
+    public void readYoutubeWebLinks() {
+        URLResource ur = new URLResource("https://www.dukelearntoprogram.com//course2/data/manylinks.html");
+        for (String s : ur.lines()) {
+            System.out.println(s);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Part4 part = new Part4();
+        part.readYoutubeWebLinks();
+    }
 }
