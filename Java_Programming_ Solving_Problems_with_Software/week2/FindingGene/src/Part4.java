bbf954446ec293d6e86df3db6508d9f4c3fcbbea
wolfeh <bill@wolfeh.info>
2022-04-27 18:44:30 -0700

    3/4. Filters out youtube links from site

--- Java_Programming_ Solving_Problems_with_Software/week2/FindingGene/src/Part4.java
+++ Java_Programming_ Solving_Problems_with_Software/week2/FindingGene/src/Part4.java
@@ -3,10 +3,34 @@ package FindingGene.src;
 import edu.duke.URLResource;
 
 public class Part4 {
+    
     public void readYoutubeWebLinks() {
         URLResource ur = new URLResource("https://www.dukelearntoprogram.com//course2/data/manylinks.html");
+        int youtubeIndex = 0;
+        int firstQuoteIndex = 0;
+        int secondQuoteIndex = 0;
+
+        // Loop through all words in the site
         for (String s : ur.lines()) {
-            System.out.println(s);
+            
+            // If the word contains “youtube.com”
+            youtubeIndex = s.indexOf("youtube.com");
+            if (youtubeIndex != -1) {
+
+                // Find the start of the link using " on the left
+                firstQuoteIndex = s.lastIndexOf("\"", youtubeIndex);
+                if (firstQuoteIndex != -1) {
+                    
+                    // Find the last " of the link on the right
+                    secondQuoteIndex = s.indexOf("\"", firstQuoteIndex + 1);
+                    if (secondQuoteIndex != -1) {
+                    
+                        // Return from the first quote to the end
+                        System.out.println(s.substring(firstQuoteIndex + 1, secondQuoteIndex));
+                    }
+                }
+            }
+            
         }
     }
 
