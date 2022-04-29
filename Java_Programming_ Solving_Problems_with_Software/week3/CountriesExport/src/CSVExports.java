fb4eacb2fc2865ea6f5b57f343f07f7c1791f0f5
wolfeh <bill@wolfeh.info>
2022-04-29 00:31:22 -0700

    Added dayLowestTemp() and dateRangeLowestTemp() methods

--- Java_Programming_ Solving_Problems_with_Software/week3/CountriesExport/src/CSVExports.java
+++ Java_Programming_ Solving_Problems_with_Software/week3/CountriesExport/src/CSVExports.java
@@ -173,22 +173,99 @@ public class CSVExports {
         return hottestRecord;
     }
 
+    public CSVRecord dayLowestTemp(CSVParser parser) {
+        String currentRecord = null;
+        double dayLowestTemp = 200.0;
+        CSVRecord coldestRecord = null;
+
+        // Loop through each record
+        for (CSVRecord record : parser) {
+            currentRecord = record.get("DateUTC") + 
+                            record.get("TemperatureF");
+
+            // Log if the highest temp for the current record
+            if (Double.parseDouble(record.get("TemperatureF")) < dayLowestTemp) {
+                dayLowestTemp = Double.parseDouble(record.get("TemperatureF"));
+                coldestRecord = record;
+            }
+        }
+        return coldestRecord;
+    }
+
+    public CSVRecord dateRangeLowestTemp() {
+        CSVRecord coldestRecord = null;
+        CSVParser parser = null;
+        double dayLowestTemp = 200.0;
+        
+        // Open a folder to select multiple .csv files with weather data
+        DirectoryResource dr = new DirectoryResource();
+        
+        // Loop through each file in the directory
+        for (File f : dr.selectedFiles()) {
+
+            // Load in the specific file and set it up with the CSV parser
+            FileResource fr = new FileResource(f);
+            parser = fr.getCSVParser();
+
+            // Get the highest temperature for current day (file)
+            CSVRecord currentLowestDayTemp = dayLowestTemp(parser);
+
+            // If it is higher than the current highest temperature'd day
+            if (Double.parseDouble(currentLowestDayTemp.get("TemperatureF")) < dayLowestTemp) {
+                // Then update the highest temp and record
+                dayLowestTemp = Double.parseDouble(currentLowestDayTemp.get("TemperatureF"));
+                coldestRecord = currentLowestDayTemp;
+            }
+        }
+        return coldestRecord;
+    }
+
+    // public double dayHighestAvgTemp(CSVParser parser) {
+    //     String currentRecord = null;
+    //     int countOfRecords = 0;
+    //     double totalTemp = 0.0;
+
+    //     // Loop through each record
+    //     for (CSVRecord record : parser) {
+    //         currentRecord = record.get("DateUTC") + 
+    //                         record.get("TemperatureF");
+            
+    //         // Sum together the total temperature across all records
+    //         totalTemp = totalTemp + Double.parseDouble(record.get("TemperatureF"));
+
+    //         // Increment the number of records
+    //         countOfRecords++;
+    //     }
+    //     return totalTemp / (double)countOfRecords;
+    // }
+
+    
+
 
     // Testing methods
     public void tester() {
-        FileResource fr = new FileResource();
-        CSVParser parser = fr.getCSVParser();
+        // FileResource fr = new FileResource();
+        // CSVParser parser = fr.getCSVParser();
         // System.out.println(countryInfo(parser, "Nauru"));
         // listExportersTwoProducts(parser, "fish", "nuts");
         // System.out.println(numberOfExporters(parser, "sugar"));
         // bigExporters(parser, "$999,999,999,999");
+
+        // Hot
         // CSVRecord dayHighestTemp = dayHighestTemp(parser);
-        System.out.println("Average hottest temp: " + dayHighestAvgTemp(parser));
-        
+        // System.out.println("Average hottest temp: " + dayHighestAvgTemp(parser));
         // System.out.println("Hottest Temp: " + dayHighestTemp.get("TemperatureF"));
         // CSVRecord dayHighestTemp = dateRangeHighestTemp();
         // System.out.println("Hottest Temp: " + dayHighestTemp.get("TemperatureF") + 
         //                    " was on " + dayHighestTemp.get("DateUTC"));
+
+        // Cold
+        // CSVRecord dayLowestTemp = dayLowestTemp(parser);
+        // System.out.println("Coldest Temp: " + dayLowestTemp.get("TemperatureF"));
+
+        CSVRecord dayLowestTemp = dateRangeLowestTemp();
+        System.out.println("Lowest Temp: " + dayLowestTemp.get("TemperatureF") + 
+                           " was on " + dayLowestTemp.get("DateUTC"));
     }
 
     // Helper methods
